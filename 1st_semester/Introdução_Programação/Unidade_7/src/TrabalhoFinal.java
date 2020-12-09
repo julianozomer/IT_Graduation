750e6cda238f21f2482df3ed2269c3981b34b041
Juliano Zomer Filho <julianozomer@hotmail.com>
2020-12-09 12:38:46 -0300

    last work

---
+++ 1st_semester/Introdução_Programação/Unidade_7/src/TrabalhoFinal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TrabalhoFinal {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+
+
+
+        teclado.close();
+    }
+}
