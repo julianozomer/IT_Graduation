cf34179c617e08502e9abd0784b4469d3c75183d
Juliano Zomer Filho <julianozomer@hotmail.com>
2023-03-16 21:29:24 -0300

    Update ListaEncadeada.java

--- 3rd_semester/AlgoritmosEEstruturasDeDados/Exercicio2/ListaEncadeada.java
+++ 3rd_semester/AlgoritmosEEstruturasDeDados/Exercicio2/ListaEncadeada.java
@@ -7,7 +7,7 @@ public class ListaEncadeada<T> {
 	}
 	
 	public NoLista<T> getPrimeiro() {
-		return primeiro;
+		return this.primeiro;
 	}
 	
 	public void inserir(T info) {
@@ -23,14 +23,80 @@ public class ListaEncadeada<T> {
 	}
 	
 	public NoLista<T> buscar(T info){
-		NoLista<T> p = primeiro;
+		NoLista<T> p = getPrimeiro();
 		while (p != null) {
-			if (p == info) {
+			if (p.getInfo().equals(info)) {
 				return p;
 			}
-			p.setProximo(p);
+			p.getProximo();
 		}
 		return null;
+	}
+	
+	public void retirar(T info) {
+		NoLista<T> anterior = null;
+		NoLista<T> p = getPrimeiro();
+		
+		while ((p != null) && (!p.getInfo().equals(info))) {
+			anterior = p;
+			p = p.getProximo();
+		}
+		
+		if (p != null) {
+			if (p == primeiro) {
+				primeiro = primeiro.getProximo();
+			} else {
+				anterior.setProximo(p.getProximo());
+			}
+		}
+	}
+	
+	public int obterComprimento() {
+		int qtdNos = 0;
+		
+		NoLista<T> p = getPrimeiro();
+		
+		while(p != null) {
+			qtdNos++;
+			p = p.getProximo();
+		}
+		
+		return qtdNos;
+	}
+	
+	public NoLista<T> obterNo(int idx){
+		if (idx < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		NoLista<T> p = getPrimeiro();
+		
+		while ((p != null) && (idx > 0)) {
+			idx--;
+			p = p.getProximo();
+		}
+		
+		if (p == null) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return p;
+	}
+	
+	public String toString() {
+		NoLista<T> p = getPrimeiro();
+		String resultado = "";
+		
+		while (p != null) {
+			if (p != getPrimeiro()) {
+				resultado += ",";
+				
+				resultado += p.getInfo().toString();
+				p = p.getProximo();
+			}
+		}
+		
+		return resultado;
 	}
 	
 	
